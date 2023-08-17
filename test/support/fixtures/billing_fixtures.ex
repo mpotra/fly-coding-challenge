9240cc3b448940290b826901673a638bfd33f797
mike <mike@mpotra.com>
2023-08-17 21:08:38 +0300

    test/support: Add Billing.InvoiceItem fixture

--- test/support/fixtures/billing_fixtures.ex
+++ test/support/fixtures/billing_fixtures.ex
@@ -8,15 +8,29 @@ defmodule Fly.BillingFixtures do
   Generate a invoice.
   """
   def invoice_fixture(organization, attrs \\ %{}) do
-    attrs = attrs
-            |> Enum.into(%{
-              due_date: ~D[2023-07-22],
-              invoiced_at: ~U[2023-07-22 12:39:00Z],
-              stripe_id: "some stripe_id"
-            })
+    attrs =
+      attrs
+      |> Enum.into(%{
+        due_date: ~D[2023-07-22],
+        invoiced_at: ~U[2023-07-22 12:39:00Z],
+        stripe_id: "some stripe_id"
+      })
 
     {:ok, invoice} = Fly.Billing.create_invoice(organization, attrs)
 
     invoice
+  end
+
+  def invoice_item_fixture(invoice, attrs \\ %{}) do
+    attrs =
+      attrs
+      |> Enum.into(%{
+        amount: 10.99,
+        description: "Some invoice item"
+      })
+
+    {:ok, invoice_item} = Fly.Billing.create_invoice_item(invoice, attrs)
+
+    invoice_item
   end
 end
