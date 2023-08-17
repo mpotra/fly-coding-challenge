0e112b7b610017158be57e414c445cad2b19f554
mike <mike@mpotra.com>
2023-08-17 17:39:12 +0300

    deps: Add Mox for mocking

--- mix.exs
+++ mix.exs
@@ -49,7 +49,10 @@ defmodule Fly.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+
+      # Development Mocks
+      {:mox, "~> 1.0", only: :test}
     ]
   end
 
